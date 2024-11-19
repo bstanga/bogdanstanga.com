501bd2f967c40f82a981d37a85c7de0c3f3205ef
Bogdan Stanga <bogydaci@gmail.com>
2024-11-19 15:43:06 +0200

    add authors

--- src/app/(posts)/ai-reviewers/page.tsx
+++ src/app/(posts)/ai-reviewers/page.tsx
@@ -47,6 +47,7 @@ export const metadata: Metadata = {
     creator: "@bdstanga",
     images: ["https://avatars.githubusercontent.com/u/3215078"],
   },
+  authors: [{ name: "Bogdan Stanga", url: "https://bogdanstanga.com" }],
 };
 
 export default function AIReviewersPost() {
@@ -430,7 +431,7 @@ function ShareButton() {
         >
           <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
         </svg>
-        Share on X (Twitter)
+        Tweet about this
       </div>
     </ExternalLink>
   );
